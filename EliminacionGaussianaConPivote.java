93214b99b70ac8f51a351b3ba6dbf7d45e6ef640
paulina-hg <23030473@itesa.edu.mx>
2025-03-10 08:22:30 -0600

    second commit

---
+++ EliminacionGaussianaConPivote.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class EliminacionGaussianaConPivote {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        int tamaño=0;
+        System.out.println("Ingrese el tamaño de la matriz ");
+        tamaño = sc.nextInt();
+
+        
+
+    }
+    
+}
