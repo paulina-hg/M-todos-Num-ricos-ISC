c8348a2df75e7c6cde4161871a6e44b71bed0b74
paulina-hg <23030473@itesa.edu.mx>
2025-03-10 08:43:44 -0600

    llenar e imprimir la matriz

--- tema3/EliminacionGaussianaConPivote.java
+++ tema3/EliminacionGaussianaConPivote.java
@@ -5,6 +5,27 @@ public class EliminacionGaussianaConPivote {
         int tamaño=0;
         System.out.println("Ingrese el tamaño de la matriz ");
         tamaño = sc.nextInt();
+        int filas= tamaño;
+        int columnas= tamaño;
+        int[][] matriz = new int[filas][columnas];
+        
+         // Llenar la matriz con un for anidado
+         System.out.println("Ingrese los valores de la matriz:");
+         for (int i = 0; i < filas; i++) {
+             for (int j = 0; j < columnas; j++) {
+                 System.out.print("Elemento [" + i + "][" + j + "]: ");
+                 matriz[i][j] = sc.nextInt();
+             }
+         }
+ 
+         // Imprimir la matriz
+         System.out.println("\nMatriz ingresada:");
+         for (int i = 0; i < filas; i++) {
+             for (int j = 0; j < columnas; j++) {
+                 System.out.print(matriz[i][j]);
+             }
+             System.out.println();
+         }
 
         
 
