b8bfef7393949de0fe900ec989773de633d121e3
paulina-hg <23030473@itesa.edu.mx>
2025-04-03 07:27:33 -0600

    terminado

--- EliminacionGaussianaConPivote.java
+++ EliminacionGaussianaConPivote.java
@@ -1,34 +1,64 @@
 import java.util.Scanner;
+
 public class EliminacionGaussianaConPivote {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tamaño=0;
-        System.out.println("Ingrese el tamaño de la matriz ");
-        tamaño = sc.nextInt();
-        int filas= tamaño;
-        int columnas= tamaño;
-        int[][] matriz = new int[filas][columnas];
-        
-         // Llenar la matriz con un for anidado
-         System.out.println("Ingrese los valores de la matriz:");
-         for (int i = 0; i < filas; i++) {
-             for (int j = 0; j < columnas; j++) {
-                 System.out.print("Elemento [" + i + "][" + j + "]: ");
-                 matriz[i][j] = sc.nextInt();
-             }
-         }
- 
-         // Imprimir la matriz
-         System.out.println("\nMatriz ingresada:");
-         for (int i = 0; i < filas; i++) {
-             for (int j = 0; j < columnas; j++) {
-                 System.out.print(matriz[i][j]);
-             }
-             System.out.println();
-         }
+        System.out.print("Ingrese el tamaño de la matriz: ");
+        int n = sc.nextInt();
+        double[][] matriz = new double[n][n + 1];
 
-        
+        System.out.println("Ingrese la matriz aumentada:");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j <= n; j++)
+                matriz[i][j] = sc.nextDouble();
 
+        System.out.println("\nMatriz aumentada inicial:");
+        imprimirMatriz(matriz);
+
+        // Eliminación Gaussiana con pivoteo parcial
+        for (int i = 0; i < n; i++) {
+            // Pivoteo parcial
+            int filaMax = i;
+            for (int k = i + 1; k < n; k++)
+                if (Math.abs(matriz[k][i]) > Math.abs(matriz[filaMax][i]))
+                    filaMax = k;
+
+            double[] temp = matriz[i];
+            matriz[i] = matriz[filaMax];
+            matriz[filaMax] = temp;
+
+            // Eliminación hacia adelante
+            for (int j = i + 1; j < n; j++) {
+                double factor = matriz[j][i] / matriz[i][i];
+                for (int k = i; k <= n; k++)
+                    matriz[j][k] -= factor * matriz[i][k];
+            }
+        }
+
+        System.out.println("\nMatriz después de eliminación:");
+        imprimirMatriz(matriz);
+
+        // Sustitución regresiva
+        double[] soluciones = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
+            double suma = 0;
+            for (int j = i + 1; j < n; j++)
+                suma += matriz[i][j] * soluciones[j];
+            soluciones[i] = (matriz[i][n] - suma) / matriz[i][i];
+        }
+
+        System.out.println("\nSoluciones:");
+        for (int i = 0; i < n; i++)
+            System.out.printf("x%d = %.4f%n", i + 1, soluciones[i]);
+
+        sc.close();
     }
-    
+
+    public static void imprimirMatriz(double[][] matriz) {
+        for (double[] fila : matriz) {
+            for (double val : fila)
+                System.out.printf("%10.4f", val);
+            System.out.println();
+        }
+    }
 }
