12de4ceba36866bba96d0dcb0a005039e2db8333
paulina-hg <23030473@itesa.edu.mx>
2025-05-27 09:43:52 -0600

    Delete Tema1 directory

--- Tema1/ErrorAbsoluto.py
+++
@@ -1,9 +0,0 @@
-class ErrorAbsoluto:
-    @staticmethod
-    def calcular(valor_real, valor_medido):
-        return abs(valor_real - valor_medido)
-
-
-if __name__ == "__main__":
-    ea = ErrorAbsoluto.calcular(2.50, 2.45)
-    print(f"Error absoluto: {ea}")
